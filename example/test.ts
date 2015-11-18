329a4f3d76cf67f86eb4ed402eb1304a119c924c
Anantha Kumaran <ananthakumaran@gmail.com>
2015-11-18 18:50:31 +0530

    add decorator example

--- example/test.ts
+++ example/test.ts
@@ -3,3 +3,27 @@ var x = 'string';
 var len = x.length;
 
 len.substring(0, 1);
+
+
+@readonly
+class Person {
+    name: string;
+    isAdmin: boolean;
+
+    constructor(name:string, admin: boolean) {
+        this.name = name;
+        this.isAdmin = admin;
+    }
+}
+
+
+function readonly<TFunction extends Function>(Target: TFunction): TFunction {
+    let newConstructor = function () {
+        Target.apply(this);
+        Object.freeze(this);
+    };
+
+    newConstructor.prototype = Object.create(Target.prototype);
+    newConstructor.prototype.constructor = Target;
+    return <any> newConstructor;
+}
