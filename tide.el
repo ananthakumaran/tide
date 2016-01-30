a68c70c3eebaa15394d96978019186b4d0a6aac2
Anantha Kumaran <ananthakumaran@gmail.com>
2016-01-30 14:13:31 +0530

    update to TS 1.8.0-beta

--- tide.el
+++ tide.el
@@ -4,7 +4,7 @@
 
 ;; Author: Anantha kumaran <ananthakumaran@gmail.com>
 ;; URL: http://github.com/ananthakumaran/tide
-;; Version: 1.7.3
+;; Version: 1.8.0-beta
 ;; Keywords: typescript
 ;; Package-Requires: ((dash "2.10.0") (flycheck "0.23") (emacs "24.1") (typescript-mode "0.1"))
 
