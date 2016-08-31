170bce9067a6467f190418284377559a9f43c667
Anantha Kumaran <ananthakumaran@gmail.com>
2016-08-31 09:50:50 +0530

    update tsserver

--- tide.el
+++ tide.el
@@ -4,7 +4,7 @@
 
 ;; Author: Anantha kumaran <ananthakumaran@gmail.com>
 ;; URL: http://github.com/ananthakumaran/tide
-;; Version: 2.0.0-beta
+;; Version: 2.0.2
 ;; Keywords: typescript
 ;; Package-Requires: ((dash "2.10.0") (flycheck "27") (emacs "24.1") (typescript-mode "0.1") (cl-lib "0.5"))
 
