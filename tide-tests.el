1673f28fefcc9a7bc0d7acea9eb22beaf708843c
katspaugh <ivan@mycs.com>
2017-05-13 13:27:46 +0200

    add tests

--- tide-tests.el
+++ tide-tests.el
@@ -22,6 +22,7 @@
     (package-install p)))
 
 (require 'tide)
+(require 'dash)
 
 ;; actual tests:
 
@@ -30,6 +31,54 @@
 
   (should (equal "this\nis\nfour\nlines"
                  (tide-normalize-lineshift "this\nis\r\nfour\nlines"))))
+
+(ert-deftest member-completetions-get-filtered ()
+  "Tests that completions get filtered by kind."
+  (let ((mock-completions
+         '(
+           (:name "DOMError" :kind "interface" :kindModifiers "declare")
+           (:name "data" :kind "property" :kindModifiers)
+           (:name "declare" :kind "keyword" :kindModifiers)
+           (:name "decode" :kind "method" :kindModifiers "declare")
+           (:name "deleteText" :kind "local function" :kindModifiers)))
+        (filtered-completions
+         '((:name "data" :kind "property" :kindModifiers)
+           (:name "decode" :kind "method" :kindModifiers "declare"))))
+    (should (-same-items?
+             (-filter
+              (lambda (completion)
+                (tide-kind-member-p (plist-get completion :kind)))
+              mock-completions)
+             filtered-completions))))
+
+(ert-deftest completions-get-sorted ()
+  "Tests that completion candidates get sorted by kind."
+  (let ((mock-completions
+         '(
+           (:name "DOMError" :kind "interface" :kindModifiers "declare")
+           (:name "data" :kind "var" :kindModifiers)
+           (:name "debugger" :kind "keyword" :kindModifiers)
+           (:name "declare" :kind "keyword" :kindModifiers)
+           (:name "decodeURI" :kind "function" :kindModifiers "declare")
+           (:name "decodeURIComponent" :kind "function" :kindModifiers "declare")
+           (:name "deleteText" :kind "local function" :kindModifiers)
+           (:name "dimensions" :kind "parameter" :kindModifiers)
+           (:name "document" :kind "var" :kindModifiers "declare")))
+        (sorted-completions
+         '(
+           (:name "dimensions" :kind "parameter" :kindModifiers)
+           (:name "data" :kind "var" :kindModifiers)
+           (:name "deleteText" :kind "local function" :kindModifiers)
+           (:name "debugger" :kind "keyword" :kindModifiers)
+           (:name "declare" :kind "keyword" :kindModifiers)
+           (:name "document" :kind "var" :kindModifiers "declare")
+           (:name "decodeURI" :kind "function" :kindModifiers "declare")
+           (:name "decodeURIComponent" :kind "function" :kindModifiers "declare")
+           (:name "DOMError" :kind "interface" :kindModifiers "declare"))
+         ))
+    (should (-same-items?
+             (-sort 'tide-compare-completions mock-completions)
+             sorted-completions))))
 
 (provide 'tide-tests)
 
