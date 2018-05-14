2ac009ffbf4f062f91e65c2ddcebf3ac3b760322
Louis-Dominique Dubeau <ldd@lddubeau.com>
2018-05-14 13:10:20 -0400

    Add tide-add-tslint-disable-next-line

--- tide-tests.el
+++ tide-tests.el
@@ -22,6 +22,7 @@
 
 (require 'tide)
 (require 'dash)
+(require 'cl)
 
 (defun tide-plist-equal (a b)
   (and (listp a) (listp b)
@@ -123,6 +124,65 @@
   (should-error (tide-load-tsconfig "test/loop.json" '()))
 
   (should-error (tide-load-tsconfig "test/notfound.json" '())))
+
+;; Adapted from jdee-mode's test suite.
+(defmacro test-with-temp-buffer (content &rest body)
+  "Fill a temporary buffer with `CONTENT' and eval `BODY' in it."
+  (declare (debug t)
+           (indent 1))
+  `(with-temp-buffer
+     (insert ,content)
+     (tide-mode)
+     (goto-char (point-min))
+     ;; We need this so that tests that simulate user actions operate on the right buffer.
+     (switch-to-buffer (current-buffer))
+     ,@body))
+
+(defun test-tide-add-tslint-disable-next-line (mock initial goto expected)
+  (test-with-temp-buffer
+   initial
+   (search-forward goto nil t)
+   (goto-char (match-beginning 0))
+   (cl-letf (((symbol-function 'tide-get-flycheck-errors-ids-at-point)
+              (lambda () mock)))
+     (tide-add-tslint-disable-next-line)
+     (should (string= (buffer-string) expected)))))
+
+(ert-deftest tide-add-tslint-disable-next-line/no-errors ()
+  "If there are no flycheck errors at point, it should be a no-op."
+  (let ((initial "// tslint:disable-next-line:previous\nconst x = 1;\nconst y = 2;\n"))
+    (test-tide-add-tslint-disable-next-line
+     ()
+     initial
+     "const y"
+     initial)))
+
+;; The first line of the buffer consitutes an edge condition. An
+;; earlier version of the function failed in this case.
+(ert-deftest tide-add-tslint-disable-next-line/first-line ()
+  "Create a new tslint flag if needed. (First line of buffer)"
+  (let ((initial "const x = 1;\nconst y = 2;\n"))
+    (test-tide-add-tslint-disable-next-line
+     '("err1" "err2")
+     initial
+     "const x"
+     (concat "// tslint:disable-next-line:err1 err2\n" initial))))
+
+(ert-deftest tide-add-tslint-disable-next-line/subsequent-lines ()
+  "Create a new tslint flag if needed. (Subsequent lines of buffer)"
+  (test-tide-add-tslint-disable-next-line
+   '("err1" "err2")
+   "const x = 1;\nconst y = 2;\n"
+   "const y"
+   "const x = 1;\n// tslint:disable-next-line:err1 err2\nconst y = 2;\n"))
+
+(ert-deftest tide-add-tslint-disable-next-line/adds ()
+  "If there was already a disable-next-line flag, add to it."
+  (test-tide-add-tslint-disable-next-line
+   '("err1" "err2")
+   "// tslint:disable-next-line:previous\nconst x = 1;\nconst y = 2;\n"
+   "const x"
+   "// tslint:disable-next-line:previous err1 err2\nconst x = 1;\nconst y = 2;\n"))
 
 (provide 'tide-tests)
 
