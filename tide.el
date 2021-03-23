2a3ac4f38472d66e2d8a6bbe5dadb52bc008acbd
Anantha Kumaran <ananthakumaran@gmail.com>
2021-03-23 22:17:57 +0530

    update tsserver

--- tide.el
+++ tide.el
@@ -4,7 +4,7 @@
 
 ;; Author: Anantha kumaran <ananthakumaran@gmail.com>
 ;; URL: http://github.com/ananthakumaran/tide
-;; Version: 4.0.5
+;; Version: 4.2.3
 ;; Keywords: typescript
 ;; Package-Requires: ((emacs "25.1") (dash "2.10.0") (s "1.11.0") (flycheck "27") (typescript-mode "0.1") (cl-lib "0.5"))
 
@@ -123,7 +123,7 @@ max-old-space-size to a higher value."
   :group 'tide)
 
 (defcustom tide-user-preferences
-  '(:includeCompletionsForModuleExports t :includeCompletionsWithInsertText t :allowTextChangesInNewFiles t)
+  '(:includeCompletionsForModuleExports t :includeCompletionsWithInsertText t :allowTextChangesInNewFiles t :generateReturnInDocTemplate t)
   "User preference plist used on the configure request.
 
 Check https://github.com/Microsoft/TypeScript/blob/17eaf50b/src/server/protocol.ts#L2684
